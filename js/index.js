ed1e9198c1d9befe2fa8da469ea0a2e365051e57
Denis Tsybin <m5221710@gmail.com>
2024-09-22 19:06:31 +0500

    сложение дробных чисел, отображение действий в subDisplay

--- js/index.js
+++ js/index.js
@@ -5,17 +5,15 @@ let result = ''
 
 let display = document.querySelector('#display')
 let subDisplay = document.querySelector('#subDisplay')
-const displayOperator = document.querySelector('#displayOperator')
 
+const displayOperator = document.querySelector('#displayOperator')
 const ce = document.querySelector('#ce')
-const mod = document.querySelector('#mod')
 const divide = document.querySelector('#divide')
 const multiply = document.querySelector('#multiply')
 const minus = document.querySelector('#minus')
 const plus = document.querySelector('#plus')
 const equal = document.querySelector('#equal')
 const comma = document.querySelector('#comma')
-
 const numbers = document.querySelectorAll('.number')
 
 ce.addEventListener('click', () => {
@@ -23,17 +21,28 @@ ce.addEventListener('click', () => {
 	secondValue = ''
 	operator = ''
 	display.value = '0'
-	subDisplay.value = ''
+	subDisplay.innerHTML = ''
+	displayOperator.classList.add('hide')
 })
 
 comma.addEventListener('click', () => {
-	if (!display.value.includes(',')) {
-		if (firstValue === '') {
-			firstValue += '0,'
+	if (operator === '') {
+		if (!firstValue.includes('.')) {
+			if (firstValue === '') {
+				firstValue = '0.'
+			} else {
+				firstValue += '.'
+			}
 			display.value = firstValue
-		} else {
-			firstValue += ','
-			display.value += ','
+		}
+	} else {
+		if (!secondValue.includes('.')) {
+			if (secondValue === '') {
+				secondValue = '0.'
+			} else {
+				secondValue += '.'
+			}
+			display.value = secondValue
 		}
 	}
 })
@@ -41,19 +50,70 @@ comma.addEventListener('click', () => {
 numbers.forEach(number => {
 	number.addEventListener('click', () => {
 		if (operator === '') {
-			if (firstValue === '') {
-				firstValue = number.id
+			if (firstValue === '' && number.id === '0') {
+				firstValue = '0'
 			} else {
 				firstValue += number.id
 			}
 			display.value = firstValue
 		} else {
-			if (secondValue === '') {
-				secondValue = number.id
+			if (secondValue === '' && number.id === '0') {
+				secondValue = '0'
 			} else {
 				secondValue += number.id
 			}
 			display.value = secondValue
 		}
 	})
+})
+
+plus.addEventListener('click', () => {
+	if (firstValue !== '') {
+		operator = 'plus'
+		subDisplay.innerHTML = firstValue + ' + '
+	}
+})
+
+minus.addEventListener('click', () => {
+	if (firstValue !== '') {
+		operator = 'minus'
+		subDisplay.innerHTML = firstValue + ' - '
+	}
+})
+
+divide.addEventListener('click', () => {
+	if (firstValue !== '' || secondValue !== '0') {
+		operator = 'divide'
+		subDisplay.innerHTML = firstValue + ' ÷ '
+	}
+})
+
+multiply.addEventListener('click', () => {
+	if (firstValue !== '') {
+		operator = 'multiply'
+		subDisplay.innerHTML = firstValue + ' × '
+	}
+})
+
+equal.addEventListener('click', () => {
+	if (firstValue !== '' && secondValue !== '') {
+		displayOperator.classList.remove('hide')
+		if (operator === 'plus') {
+			result = Number(firstValue) + Number(secondValue)
+			subDisplay.innerHTML += secondValue
+		} else if (operator === 'minus') {
+			result = Number(firstValue) - Number(secondValue)
+			subDisplay.innerHTML += secondValue
+		} else if (operator === 'divide') {
+			result = Number(firstValue) / Number(secondValue)
+			subDisplay.innerHTML += secondValue
+		} else if (operator === 'multiply') {
+			result = Number(firstValue) * Number(secondValue)
+			subDisplay.innerHTML += secondValue
+		}
+		display.value = result
+		firstValue = result
+		secondValue = ''
+		operator = ''
+	}
 })
